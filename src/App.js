d8250d4036e31238d308e408bb83474c87a9536c
basantashah <bindassbasanta@hotmail.com>
2019-01-31 21:29:15 +0545

    skipping app.js

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
+        {/* <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
             Edit <code>src/App.js</code> and save to reload.
@@ -19,7 +19,8 @@ class App extends Component {
           >
             Learn React
           </a>
-        </header>
+        </header> */}
+        <h1>Hello this is basanta</h1>
       </div>
     );
   }
