5a7c663096c1e84fbbdbc3e1e9a1e8a28bc5b530
basantashah <bindassbasanta@hotmail.com>
2019-02-22 19:23:54 +0545

    development for digital notice baord

--- src/App.js
+++ src/App.js
@@ -1,28 +1,24 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
+
+// we will comment everything we do//
 class App extends Component {
   render() {
     return (
       <div className="App">
-        {/* <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header> */}
-        <h1>Hello this is basanta</h1>
+        <h1>Digital Notice Board</h1>
+        <h2> User login page</h2>
       </div>
     );
+    // <div className="hello">
+    //   <h1> hello this is basanta</h1>
+    // </div>
+
+
+    // return React.createElement('div', { className: "App" },
+    //   React.createElement('h1', null, 'hey I am manisha'))
   }
 }
 
