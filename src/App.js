9a5b8e23f21e5f3674fd48464053feefd4d26246
basantashah <bindassbasanta@hotmail.com>
2019-02-24 10:44:46 +0545

    development

--- src/App.js
+++ src/App.js
@@ -1,15 +1,37 @@
 import React, { Component } from 'react';
-// import logo from './logo.svg';
 import './App.css';
+import Todos from './Notice/Notice';
 
 
 // I will comment everything we do//
 class App extends Component {
+  state = {
+    todos: [
+      {
+        id: 1,
+        title: 'Lets make a digital notice board',
+        completed: false
+      },
+
+      {
+        id: 2,
+        title: 'notice from RTE department',
+        completed: false
+      },
+
+
+      {
+        id: 3,
+        title: 'notice from academic department',
+        completed: false
+      }
+    ]
+  }
+
   render() {
     return (
       <div className="App">
-        <h1>Digital Notice Board</h1>
-        <h2> User login page</h2>
+        <Todos todos={this.state.todos} />
       </div>
     );
   }
