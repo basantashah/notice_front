f41b5d9b7cd805e112d6ceecac4e7965deb772c4
basantashah <bindassbasanta@hotmail.com>
2019-04-24 13:23:05 +0545

    changed ngrok to local host

--- src/config/config.js
+++ src/config/config.js
@@ -1,17 +1,17 @@
 
 
-const server='https://0c5465c8.ngrok.io/'
+const server = 'http://localhost:8000/'
 const config = {
-    
-    login: server+'api/user/login',
-    register:server+'api/auth/register',
-    notice:server+'api/notice/fetch',
-    yourNotice:server+'api/notice/yournotice',
-    postNotice:server+'api/notice/post',
-    updateNotice:server+'api/notice/update',
-    deleteNotice:server+'api/notice/delete'
- 
+
+    login: server + 'api/user/login',
+    register: server + 'api/auth/register',
+    notice: server + 'api/notice/fetch',
+    yourNotice: server + 'api/notice/yournotice',
+    postNotice: server + 'api/notice/post',
+    updateNotice: server + 'api/notice/update',
+    deleteNotice: server + 'api/notice/delete'
+
 };
- 
+
 export default config;
 
